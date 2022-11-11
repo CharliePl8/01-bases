279320100fe22c48cbfb83166599df9f5c82ff89
Carlos Barranco <carlos.barrancom@alten.es>
2022-11-11 20:03:07 +0100

    Fin del proyecto. Concluye en la sección 6

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent {
+  heroes: string[] = [
+    'SpiderMan',
+    'IronMan',
+    'Hulk',
+    'Thor',
+    'Capitán América',
+  ];
+  heroeBorrado: string = '';
+
+  borrarHeroe() {
+    this.heroeBorrado = this.heroes.shift() || '';
+  }
+}
